e5a0283306c922ed07f056510589bded43b7bb6c
spike76 <spike76@qq.com>
2022-06-02 18:25:47 +0800

    router group

--- dweb.go
+++ dweb.go
@@ -6,28 +6,34 @@ import (
 )
 
 type engine struct {
-	router router
+	groups         []*routerGroup
+	topRouterGroup *routerGroup
 }
 
 type HandlerFunc func(c *Context)
 
 func New() *engine {
-	return &engine{router: *newRouter()}
-}
-
-func (e *engine) GET(path string, handler HandlerFunc) {
-	e.router.add("GET", path, handler)
-}
-
-func (e *engine) POST(path string, handler HandlerFunc) {
-	e.router.add("POST", path, handler)
+	tg := GenTopRouterGroup()
+	e := engine{topRouterGroup: tg}
+	e.groups = append(e.groups, tg)
+	return &e
 }
 
 func (e *engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
-	e.router.handle(c)
+	e.topRouterGroup.router.handle(c)
 }
 
 func (e *engine) Run(port string) {
 	log.Fatal(http.ListenAndServe(port, e))
+}
+
+func (e *engine) TopRouterGroup() *routerGroup {
+	return e.topRouterGroup
+}
+
+func (e *engine) GenerateAndRecordRouterGroup(parent *routerGroup, prefix string) *routerGroup {
+	newGroup := parent.Group(prefix)
+	e.groups = append(e.groups, newGroup)
+	return newGroup
 }
---
+++ router_group.go
@@ -0,0 +1,31 @@
+package dweb
+
+type routerGroup struct {
+	prefix      string
+	middlewares []HandlerFunc
+	router      *router
+}
+
+func GenTopRouterGroup() *routerGroup {
+	return &routerGroup{prefix: "", router: newRouter()}
+}
+
+func (g *routerGroup) GET(pattern string, handle HandlerFunc) {
+	g.addRoute("GET", pattern, handle)
+}
+
+func (g *routerGroup) POST(pattern string, handle HandlerFunc) {
+	g.addRoute("POST", pattern, handle)
+}
+
+func (g *routerGroup) addRoute(method string, pattern string, handle HandlerFunc) {
+	g.router.add(method, g.prefix+pattern, handle)
+}
+
+func (g *routerGroup) Group(prefix string) *routerGroup {
+	cg := routerGroup{
+		prefix: g.prefix + prefix,
+		router: g.router,
+	}
+	return &cg
+}
